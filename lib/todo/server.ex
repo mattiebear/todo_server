f86f8a5a51c308ff64cb78711f1f9c3dac8484cc
Matthew Sells <matthew.sells@bonus.ly>
2024-10-05 08:38:49 -0500

    Add base server

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,12 +1,32 @@
 defmodule Todo.Server do
   use GenServer
 
+  def start do
+    GenServer.start(__MODULE__, nil)
+  end
+
+  def add(server, item) do
+    GenServer.cast(server, {:add, item})
+  end
+
+  def fetch(server, date) do
+    GenServer.call(server, {:fetch, date})
+  end
+
   @impl GenServer
   def init(_) do
     {:ok, Todo.List.new()}
   end
 
   @impl GenServer
-  def handle_call({:add, item}, _, list) do
+  def handle_cast({:add, item}, state) do
+    new_state = Todo.List.add(state, item)
+
+    {:noreply, new_state}
+  end
+
+  @impl GenServer
+  def handle_call({:fetch, date}, _from, state) do
+    {:reply, Todo.List.fetch(state, date), state}
   end
 end
