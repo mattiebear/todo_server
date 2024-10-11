2e4691dab4eefe92b021e5a4815c5e9287bd3afe
Matthew Sells <matthew.sells@proton.me>
2024-10-11 08:28:25 -0500

    Add tasks and timeout

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,9 +1,11 @@
 defmodule Todo.Server do
   use GenServer, restart: :temporary
 
-  def start_link(db_name) do
-    IO.puts("Starting server for #{db_name}...")
-    GenServer.start_link(__MODULE__, db_name, name: via_tuple(db_name))
+  @idle_timeout :timer.seconds(10)
+
+  def start_link(name) do
+    IO.puts("Starting server for #{name}...")
+    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
   end
 
   def add(server, item) do
@@ -14,24 +16,30 @@ defmodule Todo.Server do
     GenServer.call(server, {:fetch, date})
   end
 
-  defp via_tuple(db_name) do
-    Todo.ProcessRegistry.via_tuple({__MODULE__, db_name})
+  defp via_tuple(name) do
+    Todo.ProcessRegistry.via_tuple({__MODULE__, name})
   end
 
   @impl GenServer
-  def init(db_name) do
-    {:ok, {db_name, Todo.Database.get(db_name) || Todo.List.new()}}
+  def init(name) do
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}, @idle_timeout}
   end
 
   @impl GenServer
   def handle_cast({:add, item}, {name, list}) do
     new_list = Todo.List.add(list, item)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @idle_timeout}
   end
 
   @impl GenServer
   def handle_call({:fetch, date}, _from, {name, list}) do
-    {:reply, Todo.List.fetch(list, date), {name, list}}
+    {:reply, Todo.List.fetch(list, date), {name, list}, @idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_info(:timeout, {name, list}) do
+    IO.puts("Stopping server for #{name}...")
+    {:stop, :normal, {name, list}}
   end
 end
--- lib/todo/system.ex
+++ lib/todo/system.ex
@@ -1,6 +1,6 @@
 defmodule Todo.System do
   def start_link do
-    Supervisor.start_link([Todo.ProcessRegistry, Todo.Cache, Todo.Database],
+    Supervisor.start_link([Todo.Metrics, Todo.ProcessRegistry, Todo.Cache, Todo.Database],
       strategy: :one_for_one
     )
   end
